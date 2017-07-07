22185d0494b8c08e1d331cdbc6846e5f5e54e0f6
NikolayPodonin <podonin.developer@gmail.com>
2017-07-07 11:40:22 +0300

    еще один коммит

--- KudaGoWinApp/MainWindow.xaml.cs
+++ KudaGoWinApp/MainWindow.xaml.cs
@@ -40,33 +40,17 @@ namespace KudaGoWinApp
                 var response = webClient.DownloadString(@"https://kudago.com/public-api/v1.3/events/?fields=id,publication_date,dates,title,short_title,slug,place,description,body_text,location,categories,tagline,age_restriction,price,is_free,images,favorites_count,comments_count,site_url,tags,participants&expand=place,dates,location");
                 
                 var convert = JsonConvert.DeserializeObject<EventsParsing>(response);
-                //StringBuilder build = new StringBuilder();
-                //foreach (var c in convert)
-                //{
-                //    foreach (var d in c)
-                //    {
-                //        build.Append(d.Key);
-                //        build.Append(": ");
-                //        build.Append(d.Value);
-                //        build.AppendLine();
-                //    }
-                //}
-                //listBox.Items.Add(convert.results[0].short_title);
-
-                //lv_Images.Items
-
-                int i = 0;            
+                
                 foreach (Event ev in convert.results)
                 {
                     if(ev.images.Count > 0)
                     {
                         System.Windows.Controls.Image image = new System.Windows.Controls.Image();
                         Uri myUri = new Uri(ev.images[0].image);
-                        switch (myUri.Segments.Last().Substring(myUri.Segments.Last().Count() - 4, 3))
+                        switch (myUri.Segments.Last().Substring(myUri.Segments.Last().Count() - 3, 3))
                         {
                             case "jpg":
                                 {
-                                    //dec = (JpegBitmapDecoder)dec;
                                     JpegBitmapDecoder dec = new JpegBitmapDecoder(myUri, BitmapCreateOptions.None, BitmapCacheOption.Default);
                                     BitmapSource bs = dec.Frames[0];
                                     image.Source = bs;
@@ -74,7 +58,6 @@ namespace KudaGoWinApp
                                 }
                             case "peg":
                                 {
-                                    //dec = (JpegBitmapDecoder)dec;
                                     JpegBitmapDecoder dec = new JpegBitmapDecoder(myUri, BitmapCreateOptions.None, BitmapCacheOption.Default);
                                     BitmapSource bs = dec.Frames[0];
                                     image.Source = bs;
@@ -82,7 +65,6 @@ namespace KudaGoWinApp
                                 }
                             case "bmp":
                                 {
-                                    //dec = (BmpBitmapDecoder)dec;
                                     BmpBitmapDecoder dec = new BmpBitmapDecoder(myUri, BitmapCreateOptions.None, BitmapCacheOption.Default);
                                     BitmapSource bs = dec.Frames[0];
                                     image.Source = bs;
@@ -90,7 +72,6 @@ namespace KudaGoWinApp
                                 }
                             case "png":
                                 {
-                                    //dec = (PngBitmapDecoder)dec;
                                     PngBitmapDecoder dec = new PngBitmapDecoder(myUri, BitmapCreateOptions.None, BitmapCacheOption.Default);
                                     BitmapSource bs = dec.Frames[0];
                                     image.Source = bs;
@@ -98,7 +79,6 @@ namespace KudaGoWinApp
                                 }
                             case "gif":
                                 {
-                                    //dec = (GifBitmapDecoder)dec;
                                     GifBitmapDecoder dec = new GifBitmapDecoder(myUri, BitmapCreateOptions.None, BitmapCacheOption.Default);
                                     BitmapSource bs = dec.Frames[0];
                                     image.Source = bs;
@@ -106,7 +86,6 @@ namespace KudaGoWinApp
                                 }
                             case "iff":
                                 {
-                                    //dec = (TiffBitmapDecoder)dec;
                                     TiffBitmapDecoder dec = new TiffBitmapDecoder(myUri, BitmapCreateOptions.None, BitmapCacheOption.Default);
                                     BitmapSource bs = dec.Frames[0];
                                     image.Source = bs;
@@ -114,7 +93,6 @@ namespace KudaGoWinApp
                                 }
                             case "wmp":
                                 {
-                                    //dec = (WmpBitmapDecoder)dec;
                                     WmpBitmapDecoder dec = new WmpBitmapDecoder(myUri, BitmapCreateOptions.None, BitmapCacheOption.Default);
                                     BitmapSource bs = dec.Frames[0];
                                     image.Source = bs;
@@ -126,16 +104,24 @@ namespace KudaGoWinApp
                                 }
                         }
 
-                        Binding newBinding = new Binding();
-                        newBinding.ElementName = "g_Image";
-                        newBinding.Path = new PropertyPath("Image");
-                        image.SetBinding(System.Windows.Controls.Image.WidthProperty, newBinding);
+                        int n = g_Image.Children.Add(image);
 
-                       // lv_Images.Items.Add(image);
+                        Label l_Name = new Label();
+                        l_Name.Content = ev.short_title;
+                        l_Name.Margin = new Thickness(10, 20, 0, 0);
+                        
+                        Label l_Date = new Label();
+                        l_Date.Content = ev.dates[0].StartDate.Date;
+
+                        g_Image.RowDefinitions.Add(new RowDefinition());
+                        
+                        Grid.SetRow(g_Image.Children[n], n);
+                        Grid.SetRow(l_Name, n);
+                        Grid.SetRow(l_Date, n);
+
+
                     }
-                }
-
-               // lv_Images.Items.Add(image);
+                }                
             }
         }
     }
